d722033ede86a04f209dc7119459faef6b76f263
GitpreNeuR <95102789+GitpreNeuR@users.noreply.github.com>
2026-05-11 16:41:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,10 +37,12 @@ connectToDb();
 
 // Using middlewares
 // Enable CORS for multiple origins
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGINS.split(','),
+    credentials: true,
+  })
+);
 
 // JSON parsing with 50mb limit
 app.use(express.json({ limit: '50mb' }));
