720449d77a9a668bed3827c3fb5b27c61ff5dbe8
GitpreNeuR <95102789+GitpreNeuR@users.noreply.github.com>
2026-05-11 17:23:19 +0530

    cors update

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGINS.split(','),
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
